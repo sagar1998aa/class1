32d9b94188f4e23da2f1af15263c4e657b03103d
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2019-02-27 10:51:28 +1300

    class activity

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ Name=Sagar DEvkota
+ Student ID=30004439
+ */
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to toiohomai!first programming class\nhelo world ");
+            Console.WriteLine("..................");
+            Console.WriteLine("");
+
+            Console.WriteLine("..................");
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("..................");
+            Console.WriteLine("enter your age");
+            int age;
+            age = int.Parse(Console.ReadLine()); 
+            Console.ReadLine();
+       
+
+
+
+            Console.WriteLine("thank you");
+            Console.WriteLine("..................");
+            Console.ReadLine();
+        }
+    }
+}
